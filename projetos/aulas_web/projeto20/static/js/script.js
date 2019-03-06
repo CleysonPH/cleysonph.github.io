8432f79eb4a25efb9cea03ba410567da8ad3b19f
Cleyson Lima <cleysonph@gmail.com>
2019-03-05 21:08:51 -0300

    Pequenas correções

--- projetos/aulas_web/projeto20/static/js/script.js
+++ projetos/aulas_web/projeto20/static/js/script.js
@@ -1,8 +1,8 @@
 function escrever(){
-    let final = parseInt(prompt('Valor final da contagem?'))
+    var final = parseInt(prompt('Valor final da contagem?'))
 
-    let caderno = document.getElementById('caderno')
-    let caderno.innerHTML = ""
+    var caderno = document.getElementById('caderno')
+    var caderno.innerHTML = ""
 
     for (let count = 1; count <= final; count++) {
         caderno.innerHTML += count + '<br>'        
